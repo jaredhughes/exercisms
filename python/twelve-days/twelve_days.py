90cfb51b19596078fa37bbcb7ddc31c97254f219
Jared Hughes <j@redhugh.es>
2020-02-03 14:02:14 +1100

    Solve Python Twelve Days

---
+++ python/twelve-days/twelve_days.py
@@ -0,0 +1,74 @@
+def ordinal(num: int) -> str:
+    number = ""
+
+    if num == 1:
+        number = "first"
+    elif num == 2:
+        number = "second"
+    elif num == 3:
+        number = "third"
+    elif num == 4:
+        number = "fourth"
+    elif num == 5:
+        number = "fifth"
+    elif num == 6:
+        number = "sixth"
+    elif num == 7:
+        number = "seventh"
+    elif num == 8:
+        number = "eighth"
+    elif num == 9:
+        number = "ninth"
+    elif num == 10:
+        number = "tenth"
+    elif num == 11:
+        number = "eleventh"
+    elif num == 12:
+        number = "twelfth"
+    else:
+        raise Exception("Unsupported number provided for ordinal conversion.")
+
+    return number
+
+
+def first_line(day: int) -> str:
+    return "On the {} day of Christmas my true love gave to me: ".format(ordinal(day))
+
+
+def last_line(day_num: int) -> str:
+    verse = "a Partridge in a Pear Tree."
+
+    return verse if day_num == 1 else "and " + verse
+
+
+def daily_lyrics(day_num: int) -> str:
+    lyrics = [
+        "twelve Drummers Drumming, ",
+        "eleven Pipers Piping, ",
+        "ten Lords-a-Leaping, ",
+        "nine Ladies Dancing, ",
+        "eight Maids-a-Milking, ",
+        "seven Swans-a-Swimming, ",
+        "six Geese-a-Laying, ",
+        "five Gold Rings, ",
+        "four Calling Birds, ",
+        "three French Hens, ",
+        "two Turtle Doves, ",
+        last_line(day_num),
+    ]
+
+    selected_lyrics = []
+
+    for i in range(1, day_num + 1):
+        selected_lyrics.append(lyrics[-i])
+
+    return "".join(selected_lyrics[::-1])
+
+
+def recite(start_verse, end_verse):
+    lyrics = []
+
+    for day in range(start_verse, end_verse + 1):
+        lyrics.append(first_line(day) + daily_lyrics(day))
+
+    return lyrics
