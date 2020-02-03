ed430dc7f2b2df81282a511d831779c95da03085
Jared Hughes <j@redhugh.es>
2020-02-03 11:20:12 +1100

    Solve Python isogram
        URL: https://exercism.io/my/solutions/029c12ac48b14061b909a03e6da3c5e0

--- python/isogram/isogram.py
+++ python/isogram/isogram.py
@@ -1,2 +1,8 @@
-def is_isogram(string):
-    pass
+import re
+
+
+def is_isogram(string: str) -> bool:
+    char_list = list(re.sub(r'\W', '', string.lower()))
+    char_set = set(char_list)
+
+    return len(char_list) == len(char_set)
