e82c4a700d950be3efd756a2439c0778f5f914f2
Jared Hughes <j@redhugh.es>
2022-09-07 13:49:36 +0800

    Clean up Roman Numerals logic

--- typescript/roman-numerals/roman-numerals.ts
+++ typescript/roman-numerals/roman-numerals.ts
@@ -1,25 +1,25 @@
-const numberToAlpha = {
-  1: "I",
-  5: "V",
-  10: "X",
-  50: "L",
-  100: "C",
-  500: "D",
-  1000: "M",
-}
+type RomanNumeral = string;
 
-const subtractedNumbers: { [key: number]: string } = {
+const numberToAlpha: { [key: number]: RomanNumeral } = {
+  1: "I",
   4: "IV",
+  5: "V",
   9: "IX",
+  10: "X",
   40: "XL",
+  50: "L",
   90: "XC",
+  100: "C",
   400: "CD",
+  500: "D",
   900: "CM",
+  1000: "M",
 }
 
 function iterate(times: number, numeral: string): string {
   return [...Array(times)].map(_ => numeral).join("");
 }
+
 
 export const toRoman = (num: number): string => {
   let remainder = num
@@ -27,12 +27,13 @@ export const toRoman = (num: number): string => {
 
   while (remainder > 0) {
     if (remainder >= 1000) {
-      numeral = numeral + numberToAlpha[1000]
-      remainder = remainder - 1000
+      let multiples = Number(String(remainder)[0])
+      numeral = numeral + iterate(multiples, numberToAlpha[1000])
+      remainder = remainder - (multiples * 1000)
     }
 
     else if (remainder >= 900) {
-      numeral = numeral + subtractedNumbers[900]
+      numeral = numeral + numberToAlpha[900]
       remainder = remainder - 900
     }
 
@@ -42,17 +43,18 @@ export const toRoman = (num: number): string => {
     }
 
     else if (remainder >= 400) {
-      numeral = numeral + subtractedNumbers[400]
+      numeral = numeral + numberToAlpha[400]
       remainder = remainder - 400
     }
 
     else if (remainder >= 100) {
-      numeral = numeral + numberToAlpha[100]
-      remainder = remainder - 100
+      let multiples = Number(String(remainder)[0])
+      numeral = numeral + iterate(multiples, numberToAlpha[100])
+      remainder = remainder - (multiples * 100)
     }
 
     else if (remainder >= 90) {
-      numeral = numeral + subtractedNumbers[90]
+      numeral = numeral + numberToAlpha[90]
       remainder = remainder - 90
     }
 
@@ -62,7 +64,7 @@ export const toRoman = (num: number): string => {
     }
 
     else if (remainder >= 40 && remainder < 50) {
-      numeral = numeral + subtractedNumbers[40]
+      numeral = numeral + numberToAlpha[40]
       remainder = remainder - 40
     }
 
@@ -73,7 +75,7 @@ export const toRoman = (num: number): string => {
     }
 
     else if (remainder == 9) {
-      numeral = numeral + subtractedNumbers[9]
+      numeral = numeral + numberToAlpha[9]
       remainder = remainder - 9
     }
 
@@ -83,13 +85,14 @@ export const toRoman = (num: number): string => {
     }
 
     else if (remainder == 4) {
-      numeral = numeral + subtractedNumbers[4]
+      numeral = numeral + numberToAlpha[4]
       remainder = remainder - 4
     }
 
     else if (remainder > 0) {
-      numeral = numeral + numberToAlpha[1]
-      remainder = remainder - 1
+      let multiples = Number(String(remainder)[0])
+      numeral = numeral + iterate(multiples, numberToAlpha[1])
+      remainder = remainder - multiples
     }
   }
 
