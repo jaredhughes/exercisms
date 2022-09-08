9eaca443abcd8fcb8b9c8d9316e1e40e78211fcb
Jared Hughes <j@redhugh.es>
2022-09-08 18:22:32 +0800

    Solve Elixir Binary Search

--- elixir/binary-search/lib/binary_search.ex
+++ elixir/binary-search/lib/binary_search.ex
@@ -18,6 +18,23 @@ defmodule BinarySearch do
   """
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
-  def search(numbers, key) do
+  def search({}, _), do: :not_found
+  def search(numbers, key), do: search(numbers, key, 0, tuple_size(numbers) - 1)
+
+  @spec search(tuple, integer, integer, integer) :: {:ok, integer}
+  def search({}, _, _, _), do: :not_found
+  def search({key}, key, _, _), do: {:ok, 0}
+  def search(numbers, key, _, high) when key > elem(numbers, high), do: :not_found
+  def search(numbers, key, low, _) when key < elem(numbers, low), do: :not_found
+
+  def search(numbers, key, low, high) do
+    median_index = low + div(high - low, 2)
+    current_value = elem(numbers, median_index)
+
+    cond do
+      current_value == key -> {:ok, median_index}
+      key < current_value -> search(numbers, key, low, max(low, median_index - 1))
+      key > current_value -> search(numbers, key, min(median_index + 1, high), high)
+    end
   end
 end
